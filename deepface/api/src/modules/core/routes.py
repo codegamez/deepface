b7b0974f93a5bdc203655185bca3a4632dd808a9
Shahab Yousefi <shahab_yousefi@live.com>
2024-10-10 08:04:52 +0330

    .

--- deepface/api/src/modules/core/routes.py
+++ deepface/api/src/modules/core/routes.py
@@ -128,7 +128,7 @@ def verify_embeddings():
 
     logger.debug(resp_obj)
 
-    return {"success": resp_obj}
+    return {"success": bool(resp_obj)}
 
 
 @blueprint.route("/analyze", methods=["POST"])
